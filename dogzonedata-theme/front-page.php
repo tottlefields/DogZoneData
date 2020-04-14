54154de11fdbaf9cbb237d9bffea0618f62d46d5
Ellen Schofield <web@pawprintsdesign.co.uk>
2020-04-14 12:51:57 +0000

    added Tally to cause frontpage to scroll

--- dogzonedata-theme/front-page.php
+++ dogzonedata-theme/front-page.php
@@ -1,6 +1,16 @@
 <?php get_header(); ?>
 
 <div class="row">
+        <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
+                <div class="card dog mb-3">
+                        <div class="card-body">
+                                <img src="/app/wp-content/uploads/sites/2/2020/04/tally.png" class="rounded-circle float-right" alt="Tally" style="border:2px solid #008000">
+                                <h5 class="card-title" style="color: #008000">Tally</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in
+                                        to additional content.</p>
+                        </div>
+                </div>
+        </div>
 	<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
 		<div class="card dog mb-3">
 			<div class="card-body">
@@ -14,8 +24,8 @@
 	<div class="col-sm-12 col-md-6 col-lg-4 col-xl-3">
 		<div class="card dog mb-3">
 			<div class="card-body">
-				<img src="/app/wp-content/uploads/sites/2/2020/04/finnick.png" class="rounded-circle float-right" alt="Finnick" style="border:2px solid #FFA500">
-				<h5 class="card-title" style="color: #FFA500">Finnick</h5>
+				<img src="/app/wp-content/uploads/sites/2/2020/04/finnick.png" class="rounded-circle float-right" alt="Finnick" style="border:2px solid #fd7e14">
+				<h5 class="card-title" style="color: #fd7e14">Finnick</h5>
 				<p class="card-text">With supporting text below as a natural lead-in
 					to additional content.</p>
 			</div>
