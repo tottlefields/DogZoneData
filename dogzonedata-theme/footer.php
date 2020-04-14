58470df516ec0ba8ca792c79c1d4f19aae38e792
Ellen Schofield <ellen.schofield@aht.org.uk>
2020-04-14 13:55:01 +0100

    formatting

--- dogzonedata-theme/footer.php
+++ dogzonedata-theme/footer.php
@@ -1,31 +1,36 @@
-  </div>
+
+</div>
 </main>
 
-		<footer class="footer mt-auto py-3 bg-light text-center">
-<div class="container">
-  <div class="row">
-    <div class="col">
-      <a class="active" href="#"><i class="fa fa-dog fa-lg"></i><br />Dogs</a>
-    </div>
-    <div class="col">
-      <a href="#" class="text-muted"><i class="far fa-calendar-alt fa-lg"></i><br />Calendar</a>
-    </div>
-    <div class="col">
-      <a href="#" class="text-muted"><i class="fas fa-chart-line fa-lg"></i><br />Statistics</a>
-    </div>
-  </div>
-</div>
-		  </div>
-		</footer>
+<footer class="footer fixed-bottom mt-auto py-3 bg-light text-center">
+	
+<?php //require_once 'floating-action-button.php';?>
 
-		
-		<!-- start: JavaScript-->
-		<script>
+
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<a class="active" href="#"><i class="fa fa-dog fa-lg"></i><br />Dogs</a>
+			</div>
+			<div class="col">
+				<a href="#" class="text-muted"><i class="far fa-calendar-alt fa-lg"></i><br />Calendar</a>
+			</div>
+			<div class="col">
+				<a href="#" class="text-muted"><i class="fas fa-chart-line fa-lg"></i><br />Statistics</a>
+			</div>
+		</div>
+	</div>
+	</div>
+</footer>
+
+
+<!-- start: JavaScript-->
+<script>
 		var DzdAjax = {"ajax_url":"<?php echo admin_url( 'admin-ajax.php' ); ?>", "site_url":"<?php echo get_site_url(); ?>"};
 		</script>
-		<?php wp_footer(); ?>
-		
-		<script>
+<?php wp_footer(); ?>
+
+<script>
 		$(document).ready(function() {
 			$('.datepick').datepicker({
 				format: 'dd/mm/yyyy'
