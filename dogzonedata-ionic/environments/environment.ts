9e1a0ec3335844fe5de924823771b786d3faa397
Ellen Schofield <ellen@pawprintsdesign.co.uk>
2020-11-25 10:51:56 +0000

    added ionic app project to repo

---
+++ dogzonedata-ionic/environments/environment.ts
@@ -0,0 +1,26 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyAVLP3B1SuHisH8vIV0i-oQedul4rcmgyc',
+    authDomain: 'dogzonedata.firebaseapp.com',
+    databaseURL: 'https://dogzonedata.firebaseio.com',
+    projectId: 'dogzonedata',
+    storageBucket: 'dogzonedata.appspot.com',
+    messagingSenderId: '60671498033',
+    appId: '1:60671498033:web:4e0b5c571259a8770f4210',
+    measurementId: 'G-J64ELPF4TB'
+  }
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
